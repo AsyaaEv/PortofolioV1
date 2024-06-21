6dbd34d6d2cb80e97db0bf97aea9a19324fe0dba
AsyaaEv <asyaaeval@gmail.com>
2024-06-21 18:06:42 +0700

    sec commit, Fitur: Responsive Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className={`${isDarkMode ? 'dark transition duration-500 delay-500' : ''} flex w-full`}>
         <Navbar />
         <div className="dark:bg-black h-[100rem] pt-[5rem] container">
-          <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt quam ducimus commodi, quis numquam itaque voluptates nihil doloremque eligendi asperiores enim in excepturi magnam sapiente vero nisi voluptate nostrum temporibus ipsam dolorem nobis, consequuntur illum neque! Similique vel necessitatibus provident a consequatur repellendus, doloribus maxime corporis corrupti natus. Fuga, cum.</h1>
+          <h1>loli</h1>
         </div>
       </div>
     </>
